4a66ec8cabef84f5c71ca0725caa855aec6adfd3
Chinmay Shet <chishxd@tutamail.com>
2026-01-07 21:53:01 +0530

    Start working on code. Write a really basic input REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop{
+        print!("kudOS> ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+
+        println!("Command Received: {}", input.trim());
+    }
+    // println!("Hello, world!");
 }
