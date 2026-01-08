9ad3ac94e328783a03fe39978e618d2ac64d1b89
Chinmay Shet <chishxd@tutamail.com>
2026-01-08 20:21:34 +0530

    run external commands
    will start working on arguments in a minute

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     loop{
@@ -7,8 +8,23 @@ fn main() {
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read input");
+        let args : Vec<&str> = input.split_whitespace().collect();
 
-        println!("Command Received: {}", input.trim());
+        println!("Args are: {:?}", args);
+
+        execute_commands(args[0]);
     }
     // println!("Hello, world!");
+}
+
+fn execute_commands(cmd: &str){
+    let mut cmd = Command::new(cmd);
+
+    let child_proc = cmd.spawn().expect("Failed to spawn the command");
+
+    let output = child_proc.wait_with_output().expect("Failed to wait for this command");
+
+    let stdout_str = String::from_utf8_lossy(&output.stdout);
+
+    print!("{}", stdout_str);
 }
